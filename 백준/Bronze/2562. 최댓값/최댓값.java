77a2a28aba82c7b64cbeab9efc3a2132fbea6ada
DongHui-Park <122420108+DongHui-Park@users.noreply.github.com>
2023-01-26 16:32:13 +0900

    [Bronze III] Title: 최댓값, Time: 216 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[9];
+		int max = Integer.MIN_VALUE;
+		
+		for(int i=0; i<9 ; i++) {
+			arr[i] = sc.nextInt();
+			if(max < arr[i]) {
+				max = arr[i];
+			}
+		}
+		System.out.println(max);
+		for(int i=0; i<9; i++) {
+			if(max == arr[i]) {
+				System.out.println(i+1);
+			}
+		}
+	}
+}
