673b8560049e7e0b92703e0b2959790d48d0f8f9
DongHui-Park <122420108+DongHui-Park@users.noreply.github.com>
2023-02-22 12:09:50 +0900

    [D4] Title: 러시아 국기 같은 깃발, Time: 1,135 ms, Memory: 26,936 KB -BaekjoonHub

---
+++ SWEA/D4/4613. 러시아 국기 같은 깃발/러시아 국기 같은 깃발.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Solution {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+
+		int T = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= T; i++) {
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			String[][] arr = new String[N][M];
+			for (int j = 0; j < N; j++) {
+				String stt = br.readLine();
+				for (int k = 0; k < M; k++) {
+					arr[j][k] = stt.substring(k, k + 1);
+				}
+			}
+			int min = Integer.MAX_VALUE;
+			for (int j = 0; j < N - 2; j++) {
+				for (int k = j + 1; k < N - 1; k++) {
+					for (int l = k + 1; l < N; l++) {
+						int cnt = 0;
+						for (int m = 0; m < N; m++) {
+							if (m <= j) {
+								for (int n = 0; n < M; n++) {
+									if (!arr[m][n].equals("W")) {
+										cnt++;
+									}
+								}
+							} else if (m <= k) {
+								for (int n = 0; n < M; n++) {
+									if (!arr[m][n].equals("B")) {
+										cnt++;
+									}
+								}
+							} else {
+								for (int n = 0; n < M; n++) {
+									if (!arr[m][n].equals("R")) {
+										cnt++;
+									}
+								}
+							}
+						}
+						if (min > cnt) {
+							min = cnt;
+						}
+					}
+				}
+			}
+			bw.write("#" + i + " " + min);
+			bw.newLine();
+		}
+		bw.close();
+	}
+}
