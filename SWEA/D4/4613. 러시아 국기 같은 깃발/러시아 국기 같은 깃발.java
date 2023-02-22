095fad11eea1d1956ae4bcebc847feef82c70f62
DongHui-Park <122420108+DongHui-Park@users.noreply.github.com>
2023-02-22 13:52:44 +0900

    [D4] Title: 러시아 국기 같은 깃발, Time: 189 ms, Memory: 23,552 KB -BaekjoonHub

--- SWEA/D4/4613. 러시아 국기 같은 깃발/러시아 국기 같은 깃발.java
+++ SWEA/D4/4613. 러시아 국기 같은 깃발/러시아 국기 같은 깃발.java
@@ -17,44 +17,44 @@ public class Solution {
 			int N = Integer.parseInt(st.nextToken());
 			int M = Integer.parseInt(st.nextToken());
 			String[][] arr = new String[N][M];
-			for (int j = 0; j < N; j++) {
+			for (int J = 0; J < N; J++) {
 				String stt = br.readLine();
-				for (int k = 0; k < M; k++) {
-					arr[j][k] = stt.substring(k, k + 1);
+				for (int K = 0; K < M; K++) {
+					arr[J][K] = stt.substring(K, K + 1);
 				}
 			}
 			int min = Integer.MAX_VALUE;
-			for (int j = 0; j < N - 2; j++) {
-				for (int k = j + 1; k < N - 1; k++) {
-					for (int l = k + 1; l < N; l++) {
-						int cnt = 0;
-						for (int m = 0; m < N; m++) {
-							if (m <= j) {
-								for (int n = 0; n < M; n++) {
-									if (!arr[m][n].equals("W")) {
-										cnt++;
-									}
+			int j = 0;
+			for (int k = j + 1; k < N - 1; k++) {
+				for (int l = k + 1; l < N; l++) {
+					int cnt = 0;
+					for (int m = 0; m < N; m++) {
+						if (m < k) {
+							for (int n = 0; n < M; n++) {
+								if (!arr[m][n].equals("W")) {
+									cnt++;
 								}
-							} else if (m <= k) {
-								for (int n = 0; n < M; n++) {
-									if (!arr[m][n].equals("B")) {
-										cnt++;
-									}
+							}
+						} else if (m < l) {
+							for (int n = 0; n < M; n++) {
+								if (!arr[m][n].equals("B")) {
+									cnt++;
 								}
-							} else {
-								for (int n = 0; n < M; n++) {
-									if (!arr[m][n].equals("R")) {
-										cnt++;
-									}
+							}
+						} else {
+							for (int n = 0; n < M; n++) {
+								if (!arr[m][n].equals("R")) {
+									cnt++;
 								}
 							}
 						}
-						if (min > cnt) {
-							min = cnt;
-						}
+					}
+					if (min > cnt) {
+						min = cnt;
 					}
 				}
 			}
+
 			bw.write("#" + i + " " + min);
 			bw.newLine();
 		}
