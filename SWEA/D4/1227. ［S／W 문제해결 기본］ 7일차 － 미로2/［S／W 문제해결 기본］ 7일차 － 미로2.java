1da255a72764d113bc3f469dd5d445e28c924cdb
DongHui-Park <122420108+DongHui-Park@users.noreply.github.com>
2023-02-24 21:39:15 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로2, Time: 164 ms, Memory: 28,664 KB -BaekjoonHub

--- SWEA/D4/1227. ［S／W 문제해결 기본］ 7일차 － 미로2/［S／W 문제해결 기본］ 7일차 － 미로2.java
+++ SWEA/D4/1227. ［S／W 문제해결 기본］ 7일차 － 미로2/［S／W 문제해결 기본］ 7일차 － 미로2.java
@@ -46,8 +46,6 @@ public class Solution {
 					} else if (arr[mp[0] + dr[i]][mp[1] + dc[i]] == 3) {
 						tf = 1;
 						break;
-					} else {
-						arr[mp[0] + dr[i]][mp[1] + dc[i]] = 1;
 					}
 				}
 			}
