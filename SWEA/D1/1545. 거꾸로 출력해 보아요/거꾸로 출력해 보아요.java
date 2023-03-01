24e776ed3feaac15551ede058070d228f652b7ab
DongHui-Park <122420108+DongHui-Park@users.noreply.github.com>
2023-03-01 15:47:46 +0900

    [D1] Title: 거꾸로 출력해 보아요, Time: 97 ms, Memory: 17,864 KB -BaekjoonHub

---
+++ SWEA/D1/1545. 거꾸로 출력해 보아요/거꾸로 출력해 보아요.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Solution {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+		for (int i = N; 0<=i; i--) {
+			bw.write(i+" ");
+		}
+		bw.close();
+	}
+}
